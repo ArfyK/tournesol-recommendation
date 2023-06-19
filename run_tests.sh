c4af336d431d3fd502a56023aa4b01f86c129fec
Kevin Arfi <arfy.kevin@gmail.com>
2023-06-19 16:08:26 +0200

    Modified run_tests..sh so that it runs a python script and then shut down the computer

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#Run the python file
+source env/bin/activate
 
-#Shutdown the computer
+python3 temperature_tuning.py tournesol_scores_2023-05-04.csv temp_tuning_n_test=5.csv
+
 shutdown
 
 exit
