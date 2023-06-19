09997c22041b887e77ed9ddf1e8348e4fd2fd01d
Kevin Arfi <arfy.kevin@gmail.com>
2023-06-19 12:04:25 +0200

    add a rank function resembling the objective function of recommendation.py to study the coverage of the top 200 videos by random_greedy

--- temperature_tuning.py
+++ temperature_tuning.py
@@ -65,13 +65,7 @@ if len(sys.argv) < 3:  # no results file provided
 
 #### PLOTS ####
 
-
-def unique_channel(
-    df, uids
-):  # used to count how many channel are featured in each selection
-    return df.loc[df["uid"].isin(uids), "uploader"].unique().shape[0]
-
-
+# Load result dataset if necessary
 if len(sys.argv) == 3:  # results file provided
     results = pd.read_csv(sys.argv[2])
     # hack to get the uids as a python list instead of a string
@@ -95,6 +89,11 @@ for i in range(len(X)):
     axs[i % 3, i % 4].set_ylabel("")
 
 # Number of different channel featured in the selection:
+def unique_channel(
+    df, uids
+):  # used to count how many channel are featured in each selection
+    return df.loc[df["uid"].isin(uids), "uploader"].unique().shape[0]
+
 n_vid_per_recommendation = len(results.loc[1, "uids"])
 results["n_channel"] = results["uids"].apply(lambda x: unique_channel(df, x))
 
@@ -121,15 +120,15 @@ plt.subplots_adjust(
 
 plt.savefig(fname="temperature_criteria_comparison.png")
 
-# Coverage of the top K
+# Coverage of the top K of tournesol scores
 K = 200
 
 algo_list = list(results["algorithm"].unique())
 
 coverage = pd.DataFrame(columns=["uid", "rank"] + algo_list)
 coverage["uid"] = list(
-    df.sort_values(by="largely_recommended", ascending=False)["uid"].iloc[0:200]
-)  # we could also rank the videos according to our objective function instead of looking only at the tournesol score
+    df.sort_values(by="largely_recommended", ascending=False)["uid"].iloc[0:K]
+)
 
 coverage["rank"] = list(range(1, K + 1))
 coverage[algo_list] = np.zeros((K, len(algo_list)))
@@ -145,6 +144,39 @@ def compute_coverage(coverage_df, result_series):
         + 1
     )
 
+results.apply(lambda x: compute_coverage(coverage, x), axis=1)
+coverage[algo_list] = coverage[algo_list] * len(algo_list) / results.size
+
+f, axs = plt.subplots(3, 2, figsize=(13, 7), sharex=True, sharey=True)
+for i in range(len(algo_list)):
+    sns.barplot(data=coverage, x="rank", y=algo_list[i], ax=axs[i % 3, i % 2])
+    axs[i % 3, i % 2].axhline(y=results["test"].max() / 200)
+    axs[i % 3, i % 2].set_title(algo_list[i])
+    axs[i % 3, i % 2].yaxis.set_label_text("count")
+
+f.suptitle('Coverage of the top '+str(K)+' tournesol scores')
+plt.subplots_adjust(
+    left=0.055, bottom=0.076, right=0.994, top=0.907, wspace=0.072, hspace=0.238
+)
+
+plt.savefig(fname="temperature_coverage_tournesolscores.png")
+
+#Coverage of top K ranking the videos with a function resembling the objective function
+
+def aggregated_score(video_scores_series, l = 1/10, alpha=1/2):
+    tournesol_score = video_scores_series[CRITERIA[0]]
+    criteria_aggregation = l*video_scores_series[CRITERIA[1:]].apply(lambda x: x**alpha).sum()
+    return tournesol_score + criteria_aggregation
+
+df['aggregated_score'] = df.apply(aggregated_score, axis='columns')
+
+coverage = pd.DataFrame(columns=["uid", "rank"] + algo_list)
+coverage["uid"] = list(
+    df.sort_values(by='aggregated_score', ascending=False)["uid"].iloc[0:K]
+)
+
+coverage["rank"] = list(range(1, K + 1))
+coverage[algo_list] = np.zeros((K, len(algo_list)))
 
 results.apply(lambda x: compute_coverage(coverage, x), axis=1)
 coverage[algo_list] = coverage[algo_list] * len(algo_list) / results.size
@@ -156,8 +188,9 @@ for i in range(len(algo_list)):
     axs[i % 3, i % 2].set_title(algo_list[i])
     axs[i % 3, i % 2].yaxis.set_label_text("count")
 
+f.suptitle('Coverage of the top '+str(K)+' aggregated scores')
 plt.subplots_adjust(
-    left=0.055, bottom=0.076, right=0.994, top=0.946, wspace=0.072, hspace=0.22
+    left=0.055, bottom=0.076, right=0.994, top=0.907, wspace=0.072, hspace=0.238
 )
 
-plt.savefig(fname="temperature_coverage_comparison.png")
+plt.savefig(fname="temperature_coverage_aggregatedscore.png")
