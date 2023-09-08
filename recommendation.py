aba2686c3807bd5aa743237d92db49674cbcc231
Kevin Arfi <arfy.kevin@gmail.com>
2023-09-08 11:04:23 +0200

    Add a new term in objective functions penalizing old videos. Adapt the algorithms to the new objective functions.

--- recommendation.py
+++ recommendation.py
@@ -19,11 +19,25 @@ CRITERIA = [
     "backfire_risk",
 ]
 
+
+def get_age_in_days(video_series, ref_date):
+    # return 1 if the video is less than a day old
+    return max(
+        (
+            ref_date
+            - datetime.datetime.strptime(
+                video_series["publication_date"].split("T")[0], "%Y-%m-%d"
+            )
+        ).days,
+        1,
+    )  # remove the time part of the datetime with the split because some entries only have the date part.
+
+
 ### Greedy objective functions
 
 
 # Complete Objective function
-def F(partial_sums, new, l=1 / 10, alpha=0.5):
+def F(partial_sums, new, ref_date, l=1 / 10, alpha=0.5, mu=0.1):
     # Relevance term
     R = partial_sums["largely_recommended"] + new["largely_recommended"]
     # Diversity term
@@ -32,15 +46,28 @@ def F(partial_sums, new, l=1 / 10, alpha=0.5):
         .apply(lambda x: x ** (alpha))
         .sum()
     )
-    return R + l * C
+
+    # Recency term
+    D = (
+        partial_sums["age_in_days_inverses"] + 1 / get_age_in_days(new, ref_date)
+    ) ** alpha
+
+    return R + l * C + mu * D
 
 
-# Incomplete objective function, only based on the tournesol score
-def R(partial_sum, new):
-    return partial_sum + new["largely_recommended"]
+# Incomplete objective function
+def F_incomplete(partial_sums, new, ref_date, alpha=0.5, mu=0.1):
+    # Relevance term
+    R = partial_sums["largely_recommended"] + new["largely_recommended"]
+    # Recency term
+    D = (
+        partial_sums["age_in_days_inverses"] + 1 / get_age_in_days(new, ref_date)
+    ) ** alpha
+
+    return R + mu * D
 
 
-def deterministic_greedy(data, n_vid=10, q=0.15, l=1 / 10, alpha=0.5):
+def deterministic_greedy(data, ref_date, n_vid=10, q=0.15, l=1 / 10, alpha=0.5, mu=0.1):
     df = data.copy()  # copy the dataframe to avoid modifying the original
 
     # Normalizes the dataframe
@@ -59,28 +86,33 @@ def deterministic_greedy(data, n_vid=10, q=0.15, l=1 / 10, alpha=0.5):
 
     # Selection of videos scored according to all criteria
     S1 = []  # uids of the selected videos
-    partial_sums = pd.Series(data=[0] * len(CRITERIA), index=CRITERIA)
+    index = CRITERIA + ["age_in_days_inverses"]
+    partial_sums = pd.Series(data=[0] * len(index), index=index)
 
     for i in range(n_complete):
         # Compute the objective function
         obj = df.loc[~df["uid"].isin(S1)].apply(
-            lambda x: F(partial_sums, x, l, alpha), axis="columns"
+            lambda x: F(partial_sums, x, ref_date, l, alpha, mu), axis="columns"
         )
 
         # Update S1 and partial sums
         new = df.at[obj.idxmax(), "uid"]
         S1.append(new)
-        partial_sums = (partial_sums + df.loc[df["uid"] == new, CRITERIA]).iloc[
+        partial_sums[CRITERIA] = (
+            partial_sums[CRITERIA] + df.loc[df["uid"] == new, CRITERIA]
+        ).iloc[
             0
         ]  # hack to keep a series
+        partial_sums["age_in_days_inverses"] = partial_sums[
+            "age_in_days_inverses"
+        ] + 1 / get_age_in_days(df.loc[df["uid"] == new].iloc[0], ref_date)
         objective1 = obj.max()
 
     # Selection of videos only using the tournesol score
     S2 = []  # indexes of the selected videos
-    partial_sum = 0
-    df_incomplete = df.loc[
-        contains_na, ["uid", "largely_recommended"]
-    ]  # Remove videos with no missing score
+    index = ["largely_recommended", "age_in_days_inverses"]
+    partial_sums = pd.Series(data=[0] * len(index), index=index)
+    df_incomplete = df.loc[contains_na]  # Remove videos with no missing score
     df_incomplete = df_incomplete[
         ~df_incomplete["uid"].isin(S1)
     ]  # Remove videos that were already selected
@@ -92,19 +124,21 @@ def deterministic_greedy(data, n_vid=10, q=0.15, l=1 / 10, alpha=0.5):
     for i in range(n_incomplete):
         # Compute the objective function
         obj = df_incomplete.loc[~df_incomplete["uid"].isin(S2)].apply(
-            lambda x: R(partial_sum, x), axis="columns"
+            lambda x: F_incomplete(partial_sums, x, ref_date, alpha, mu), axis="columns"
         )
 
         # Update S2 and partial sums
         new = df_incomplete.loc[obj.idxmax(), "uid"]
         S2.append(new)
-        partial_sum = (
-            partial_sum
+        partial_sums["largely_recommended"] = (
+            partial_sums["largely_recommended"]
             + df_incomplete.loc[df_incomplete["uid"] == new, "largely_recommended"]
         ).iloc[
             0
         ]  # hack to get a series
-
+        partial_sums["age_in_days_inverses"] = partial_sums[
+            "age_in_days_inverses"
+        ] + 1 / get_age_in_days(df.loc[df["uid"] == new].iloc[0], ref_date)
     objective2 = obj.max()
 
     return {"uids": S1 + S2, "obj": objective1 + objective2}
@@ -122,11 +156,13 @@ def aggregated_score(series, l, alpha):
 
 def deterministic_random_sample(
     data,
+    ref_date,
     sample_size,
     n_vid=10,
     q=0.15,
     l=1 / 10,
     alpha=0.5,
+    mu=0.1,
     quantile=0,
     key=rank_by_tournesol_score,
 ):
@@ -140,105 +176,20 @@ def deterministic_random_sample(
     sample.reset_index(drop=True, inplace=True)
 
     return deterministic_greedy(
-        sample[CRITERIA + ["uid"]], n_vid=n_vid, alpha=alpha, l=l, q=q
+        sample, ref_date, n_vid=n_vid, alpha=alpha, l=l, mu=mu, q=q
     )
 
 
-def random_greedy(data, n_vid=10, q=0.15, l=1 / 10, alpha=0.5, T=1):
-    df = data.copy()  # copy the dataframe to avoid modifying the original
-
-    # Normalizes the dataframe
-    contains_na = df[CRITERIA].isna().apply(any, axis="columns")
-
-    df[CRITERIA] = df[CRITERIA] - df[CRITERIA].min()
-    df = df.fillna(0)
-
-    # Determine how many videos with and without na will be selected
-    n_incomplete = int(q * n_vid)
-    incomplete_available = contains_na.sum()
-
-    if incomplete_available < n_incomplete:
-        n_incomplete = incomplete_available
-
-    n_complete = n_vid - n_incomplete
-
-    # Selection of videos scored according to all criteria
-    S1 = []  # uids of the selected videos
-    partial_sums = pd.Series(data=[0] * len(CRITERIA), index=CRITERIA)
-
-    for i in range(n_complete):
-        # Compute the objective function
-        obj = df.loc[~df["uid"].isin(S1)].apply(
-            lambda x: F(partial_sums, x, l, alpha), axis="columns"
-        )
-        # Compute the probability distribution
-        p = obj.divide(obj.mean()).apply(
-            lambda x: np.exp(x / T)
-        )  # objective value are normalized
-        norm = p.sum()
-        p = p.apply(lambda x: x / norm)
-
-        # sample a new element from p
-        new_idx = np.random.choice(a=obj.index, size=1, replace=False, p=p)[0]
-        new = df.loc[new_idx, "uid"]
-
-        # Update S1 and partial sums
-        S1.append(new)
-        partial_sums = (partial_sums + df.loc[df["uid"] == new, CRITERIA]).iloc[
-            0
-        ]  # hack to keep a series
-
-    objective1 = obj[new_idx]
-
-    # Selection of videos only using the tournesol score
-    S2 = []  # indexes of the selected videos
-    partial_sum = 0
-    df_incomplete = df.loc[
-        contains_na, ["uid", "largely_recommended"]
-    ]  # Remove videos with no missing score
-    df_incomplete = df_incomplete[
-        ~df_incomplete["uid"].isin(S1)
-    ]  # Remove videos that were already selected
-
-    for i in range(n_incomplete):
-        # Compute the objective function
-        obj = df_incomplete.loc[~df_incomplete["uid"].isin(S2)].apply(
-            lambda x: R(partial_sum, x), axis="columns"
-        )
-
-        # Compute the probability distribution
-        p = obj.divide(obj.mean()).apply(
-            lambda x: np.exp(x / T)
-        )  # objective value are normalized
-        norm = p.sum()
-        p = p.apply(lambda x: x / norm)
-
-        # sample a new element from p
-        new_idx = np.random.choice(a=obj.index, size=1, replace=False, p=p)[0]
-        new = df_incomplete.loc[new_idx, "uid"]
-
-        # Update S2 and partial sums
-        S2.append(new)
-        partial_sum = (
-            partial_sum
-            + df_incomplete.loc[df_incomplete["uid"] == new, "largely_recommended"]
-        ).iloc[
-            0
-        ]  # hack to get a series
-
-    objective2 = obj[new_idx]
-
-    return {"uids": S1 + S2, "obj": objective1 + objective2}
-
-
-### Random sampling
+### Uniformly Random sampling
 
 
 def random(
     data,
+    ref_date,
     n_vid=10,
     alpha=0.5,
     l=1 / 10,
+    mu=0.1,
     pre_selection=False,
     quantile=0,
     key=rank_by_tournesol_score,
@@ -262,6 +213,11 @@ def random(
     obj = (
         sums["largely_recommended"]
         + l * sums[CRITERIA[1:]].apply(lambda x: x**alpha).sum()
+        + mu
+        * selection.apply(
+            lambda x: 1 / get_age_in_days(x, ref_date), axis="columns"
+        ).sum()
+        ** alpha
     )
 
     return {"uids": S, "obj": obj}
@@ -304,15 +260,17 @@ if __name__ == "__main__":
 
     #### TESTS ####
     if len(sys.argv) < 3:  # no results file provided
-        n_tests = 10
+        n_tests = 1
 
         size = 100
 
         alpha = 0.5  # exponent of the power function used in the objective function
 
-        T = 80  # temperature used in random_greedy
+        mu = 0.1
 
-        n_vid = 5
+        ref_date = datetime.datetime(2023, 5, 10, 0, 0)
+
+        n_vid = 12
 
         results = []
 
@@ -324,23 +282,29 @@ if __name__ == "__main__":
             ]
             maxs = (df[CRITERIA] - df[CRITERIA].min()).max()
 
-            dg = deterministic_greedy(df, n_vid=n_vid, alpha=alpha, l=1 / 10)
+            dg = deterministic_greedy(
+                df, ref_date, n_vid=n_vid, alpha=alpha, l=1 / 10, mu=mu
+            )
             maxs_dg = (
                 df.loc[df["uid"].isin(dg["uids"]), CRITERIA]
                 .max()
                 .divide(maxs)
                 .to_list()
             )
-            results.append([k + 1, "dg_l=1/10", alpha, dg["uids"], dg["obj"]] + maxs_dg)
+            results.append(
+                [k + 1, "dg_l=1/10", alpha, mu, dg["uids"], dg["obj"]] + maxs_dg
+            )
 
             dg_random_sample = deterministic_random_sample(
                 df,
+                ref_date,
+                sample_size=90,
                 n_vid=n_vid,
                 q=0.15,
                 l=1 / 10,
                 alpha=alpha,
-                n_sample=50,
-                quantile=0.5,
+                mu=mu,
+                quantile=0.75,
                 key=rank_by_tournesol_score,
             )
             maxs_dg_random_sample = (
@@ -352,27 +316,23 @@ if __name__ == "__main__":
             results.append(
                 [
                     k + 1,
-                    "dg_random_sample_l=1/10",
+                    "dg_random_sample_l=1/10_size=50",
                     alpha,
+                    mu,
                     dg_random_sample["uids"],
                     dg_random_sample["obj"],
                 ]
                 + maxs_dg_random_sample
             )
 
-            rg = random_greedy(df, n_vid=n_vid, alpha=alpha, l=1 / 10, T=T)
-            maxs_rg = (
-                df.loc[df["uid"].isin(rg["uids"]), CRITERIA]
-                .max()
-                .divide(maxs)
-                .to_list()
-            )
-            results.append(
-                [k + 1, "rg_l=1/10_T=80", alpha, rg["uids"], rg["obj"]] + maxs_rg
-            )
-
             r_75 = random(
-                df, n_vid=n_vid, alpha=alpha, pre_selection=True, quantile=0.75
+                df,
+                ref_date,
+                n_vid=n_vid,
+                alpha=alpha,
+                mu=mu,
+                pre_selection=True,
+                quantile=0.75,
             )
             maxs_75 = (
                 df.loc[df["uid"].isin(r_75["uids"]), CRITERIA]
@@ -385,6 +345,7 @@ if __name__ == "__main__":
                     k + 1,
                     "r_75",
                     alpha,
+                    mu,
                     r_75["uids"],
                     r_75["obj"],
                 ]
@@ -392,7 +353,14 @@ if __name__ == "__main__":
             )
 
         # Set up a dataframe to hold the results
-        columns = ["test", "algorithm", "alpha", "uids", "objective_value"] + CRITERIA
+        columns = [
+            "test",
+            "algorithm",
+            "alpha",
+            "mu",
+            "uids",
+            "objective_value",
+        ] + CRITERIA
         results = pd.DataFrame(data=results, columns=columns).set_index("test")
 
         results.to_csv(
